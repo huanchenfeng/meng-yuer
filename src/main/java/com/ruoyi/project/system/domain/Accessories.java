b1d1506a4ecf10a649f20acdb3e44ec6a8b16476
cfs <2733828240@qq.com>
2024-10-29 17:09:42 +0800

    增加我的饰品

--- src/main/java/com/ruoyi/project/system/domain/Accessories.java
+++ src/main/java/com/ruoyi/project/system/domain/Accessories.java
@@ -2,6 +2,7 @@ package com.ruoyi.project.system.domain;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -22,66 +23,87 @@ public class Accessories
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("饰品ID")
+    @JsonAlias("id")
     private Integer id;
 
     @ApiModelProperty("BUFF平台饰品ID")
+    @JsonAlias("buff_id")
     private Integer buffId;
 
     @ApiModelProperty("BUFF在售价")
+    @JsonAlias("buff_sell_price")
     private BigDecimal buffSellPrice;
 
     @ApiModelProperty("BUFF在售数量")
+    @JsonAlias("buff_sell_num")
     private Integer buffSellNum;
 
     @ApiModelProperty("BUFF求购价")
+    @JsonAlias("buff_buy_price")
     private BigDecimal buffBuyPrice;
 
     @ApiModelProperty("BUFF求购数量")
+    @JsonAlias("buff_buy_num")
     private Integer buffBuyNum;
 
     @ApiModelProperty("Steam在售价")
+    @JsonAlias("steam_sell_price")
     private BigDecimal steamSellPrice;
 
     @ApiModelProperty("Steam在售数量")
+    @JsonAlias("steam_sell_num")
     private Integer steamSellNum;
 
     @ApiModelProperty("Steam求购价")
+    @JsonAlias("steam_buy_price")
     private BigDecimal steamBuyPrice;
 
     @ApiModelProperty("Steam求购数量")
+    @JsonAlias("steam_buy_num")
     private Integer steamBuyNum;
 
     @ApiModelProperty("Steam市场名称")
+    @JsonAlias("market_hash_name")
     private String marketHashName;
 
     @ApiModelProperty("饰品中文名称")
+    @JsonAlias("name")
     private String name;
 
     @ApiModelProperty("饰品图片")
+    @JsonAlias("img")
     private String img;
 
     @ApiModelProperty("YYYP平台饰品ID")
+    @JsonAlias("yyyp_id")
     private Integer yyypId;
 
     @ApiModelProperty("YYYP在售价")
+    @JsonAlias("yyyp_sell_price")
     private BigDecimal yyypSellPrice;
 
     @ApiModelProperty("YYYP在售数量")
+    @JsonAlias("yyyp_sell_num")
     private Integer yyypSellNum;
 
     @ApiModelProperty("YYYP求购价")
+    @JsonAlias("yyyp_buy_price")
     private BigDecimal yyypBuyPrice;
 
     @ApiModelProperty("YYYP求购数量")
+    @JsonAlias("yyyp_buy_num")
     private Integer yyypBuyNum;
 
     @ApiModelProperty("平台价格最大值")
+    @JsonAlias("max_price")
     private BigDecimal maxPrice;
 
     @ApiModelProperty("Steam当日成交量")
+    @JsonAlias("turnover_number")
     private BigDecimal turnoverNumber;
 
     @ApiModelProperty("Steamid")
+    @JsonAlias("steam_id")
     private Integer steamId;
 
 }
---
+++ src/main/java/com/ruoyi/project/system/domain/MyAccessories.java
@@ -0,0 +1,10 @@
+package com.ruoyi.project.system.domain;
+
+import lombok.Data;
+
+@Data
+public class MyAccessories extends Accessories{
+    private Integer sum;
+
+
+}
